8eabd7b59558c9b1c9d33c4b30031865d7af6c9a
Eric Muxagata <ericgmconrado@gmail.com>
2013-11-30 23:10:45 -0200

    Nothing to say in my defense.

---
+++ master/src/com/paerentoop/master/dummy/DumbMaster.java
@@ -0,0 +1,11 @@
+package com.paerentoop.master.dummy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ericmuxagata
+ * Date: 11/30/13
+ * Time: 11:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DumbMaster {
+}
