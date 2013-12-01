fcd9caf34a86d727ce8ea027e79c94840d37a6a3
Eric Muxagata <ericgmconrado@gmail.com>
2013-11-30 22:27:45 -0200

    Initial commit with .gitignore and multiple projects (Slave and Master)

---
+++ master/src/networking/api/MessageDispatcher.java
@@ -0,0 +1,11 @@
+package networking.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ericmuxagata
+ * Date: 11/30/13
+ * Time: 10:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageDispatcher {
+}
---
+++ slave/src/com/parentoop/slave/model/SlaveStates.java
@@ -0,0 +1,11 @@
+package com.parentoop.slave.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ericmuxagata
+ * Date: 11/30/13
+ * Time: 10:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum SlaveStates {
+}
