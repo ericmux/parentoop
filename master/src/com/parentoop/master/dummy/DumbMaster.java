701468837d71948d60eff84ce13d935917318185
Bernardo Rufino <contato@bernardorufino.com.br>
2013-12-01 00:32:20 -0200

    No file headers.

--- master/src/com/parentoop/master/dummy/DumbMaster.java
+++ master/src/com/parentoop/master/dummy/DumbMaster.java
@@ -1,11 +1,4 @@
 package com.parentoop.master.dummy;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ericmuxagata
- * Date: 11/30/13
- * Time: 11:10 PM
- * To change this template use File | Settings | File Templates.
- */
 public class DumbMaster {
 }
