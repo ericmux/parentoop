9d4154048d3bd7858fa268899a36dc55c51a9377
Victor Elias <victorgelias@gmail.com>
2013-12-04 06:41:01 -0200

    Creating Mapping execution phase on master
    On the mapping phase we read the input and send it to slaves for mapping
    to be done on the slave machines.

--- master/src/com/parentoop/master/core/application/MapReduceTaskExecution.java
+++ master/src/com/parentoop/master/core/application/MapReduceTaskExecution.java
@@ -2,8 +2,8 @@ package com.parentoop.master.core.application;
 
 import com.google.common.collect.Lists;
 import com.parentoop.core.loader.Task;
+import com.parentoop.master.core.application.phases.MappingPhase;
 import com.parentoop.master.core.application.phases.SetupPhase;
-import com.parentoop.master.core.execution.ExecutionPhase;
 import com.parentoop.master.core.execution.TaskExecution;
 
 import java.nio.file.Path;
@@ -11,6 +11,6 @@ import java.nio.file.Path;
 public class MapReduceTaskExecution extends TaskExecution<Path> {
 
     public MapReduceTaskExecution(Path inputPath, Task task, TaskExecutionListener<Path> listener) {
-        super(task, Lists.<ExecutionPhase<Path>>newArrayList(new SetupPhase()), listener);
+        super(task, Lists.newArrayList(new SetupPhase(), new MappingPhase(inputPath)), listener);
     }
 }
