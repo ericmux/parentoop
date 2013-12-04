7d47e42f555f14ae2f0ff0a9ca17ed47773a7a61
Victor Elias <victorgelias@gmail.com>
2013-12-04 06:33:40 -0200

    Creating MapReduceTaskExecution stub
    Since the Task doens't provide a getInputPath method anymore, the map
    reduce task execution will receive the input path on its construtor and
    forward it to the constructor of one of the execution phases (the mapping
    one)

---
+++ master/src/com/parentoop/master/core/application/MapReduceTaskExecution.java
@@ -0,0 +1,15 @@
+package com.parentoop.master.core.application;
+
+import com.google.common.collect.Lists;
+import com.parentoop.core.loader.Task;
+import com.parentoop.master.core.execution.ExecutionPhase;
+import com.parentoop.master.core.execution.TaskExecution;
+
+import java.nio.file.Path;
+
+public class MapReduceTaskExecution extends TaskExecution<Path> {
+
+    public MapReduceTaskExecution(Path inputPath, Task task, TaskExecutionListener<Path> listener) {
+        super(task, Lists.<ExecutionPhase<Path>>newArrayList(), listener);
+    }
+}
